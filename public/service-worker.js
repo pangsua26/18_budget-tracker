1ed0fbd0987bc6db54cc9c8505e330d1d15e8000
Pangsua Vang <pangsua26@yahoo.com>
2020-08-05 14:23:45 -0500

    activate

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,7 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-//install
+// install
 self.addEventListener("install", function(evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -23,3 +23,21 @@ self.addEventListener("install", function(evt) {
 
   self.skipWaiting();
 });
+
+// activate
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
