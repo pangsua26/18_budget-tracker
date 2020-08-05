b298e17c5e6722e647d411c25ae31e37a3b011f8
Pangsua Vang <pangsua26@yahoo.com>
2020-08-05 14:11:08 -0500

    files to sv

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,10 @@
 const FILES_TO_CACHE = [
-    
+    "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
+    "/manifest.webmanifest",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
 ]
