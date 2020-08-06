f04814aeadd7bbbd37bd00d49ab167ae18c6fd83
Pangsua Vang <pangsua26@yahoo.com>
2020-08-06 15:05:19 -0500

    added script and fixed errors

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,7 +3,7 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/index.js",
     "/db.js",
-    "/manifest.json",
+    "/manifest.webmanifest",
     "/styles.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
